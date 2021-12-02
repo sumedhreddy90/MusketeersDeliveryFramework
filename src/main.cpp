0754b93b3a63461912becdbf44c1788ca4a32c69
sumedhreddy90 <sumedhreddykoppula@gmail.com>
2021-12-02 16:01:01 -0500

    [Task SRK104] Writing code for invoking MuskyRviz with PyQt QApplication

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
+#include <ros/ros.h>
 #include "muskyRviz.h"
+#include <QApplication>
 
 int main(int argc, char **argv)
 {
+  if( !ros::isInitialized() )
+  {
+    ros::init( argc, argv, "muskyRviz", ros::init_options::AnonymousName );
+  }
+  // TODO: Integrate QT5 Application with ROS
+  QApplication app( argc, argv );
 
+  MuskyRviz* muskyRviz = new muskyRviz();
+  muskyRviz->show();
+
+  app.exec();
+
+  delete muskyRviz;
 }
